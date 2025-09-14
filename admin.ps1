4ea7f1f4e37d5cb8a9249b4321dcf18b70606e87
Cahtherine1404 <huyvu.hw@gmail.com>
2025-09-14 23:09:15 +0700

    first commit

---
+++ admin.ps1
@@ -0,0 +1,15 @@
+# change directary to current user
+cd ~
+
+# install cursor hightligter
+winget install 9N8CMVV4H268
+
+# install choco
+winget install Chocolatey.Chocolatey
+
+# install Unikey
+choco --version
+choco install Unikey -y
+
+# install office
+choco install microsoft-office-deployment -y
---
+++ user.ps1
@@ -0,0 +1,75 @@
+# change directary to current user
+cd ~
+
+# install scoop
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+  Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+  irm get.scoop.sh | iex
+}
+
+#install git
+scoop install git
+
+# add bucket
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add nerd-fonts
+
+# install font
+scoop install JetBrainsMono-NF
+
+# install which
+scoop install which
+
+# install fuzzy finder
+scoop install fzf
+
+# install touch
+scoop install touch
+
+# install python
+scoop install python
+
+# install nodejs
+scoop install nodejs
+
+# install gcc
+scoop install gcc
+
+# install java
+scoop install openjdk
+
+# install sublime text
+scoop install sublime-text
+
+# install xampp
+scoop install xampp
+
+# install vscode
+scoop install vscode
+
+# install chrome
+scoop install googlechrome
+
+# install brave
+scoop install brave
+
+# install lua
+scoop install lua
+scoop install luarocks
+
+# install neovim
+scoop install neovim
+cd AppData/Local
+git clone https://github.com/Catherine1401/nvim.git
+
+# instal android studio
+scoop install android-studio
+
+# install flutter
+scoop install flutter
+flutter doctor --android-licenses
+
+# install color picker
+scoop install jcpicker
+
